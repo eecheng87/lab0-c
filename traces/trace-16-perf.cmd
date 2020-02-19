690e2e66344e8f15fbf2ef44d29faa0c49cd44a8
eecheng <yucheng871011@gmail.com>
2020-02-19 10:05:23 +0800

    Modify sort algorithmn

--- traces/trace-16-perf.cmd
+++ traces/trace-16-perf.cmd
@@ -1,3 +1,4 @@
+
 # Test performance of sort with random and descending orders
 # 10000: all correct sorting algorithms are expected pass
 # 50000: sorting algorithms with O(n^2) time complexity are expected failed
